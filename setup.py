5abc961d5a03a203d772bb0441731402532c8dc1
Pilar Marinas <pilar.marinas@upcnet.es>
2014-04-09 11:12:22 +0200

    Canvis estils, fonts i logo

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+README = open("README.rst").read()
+HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
+
+setup(name='vilaix.santaperpetuamogoda',
+      version=version,
+      description="",
+      long_description=README + "\n" + HISTORY,
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='UPCnet Plone Team',
+      author_email='plone.team@upcnet.es',
+      url='https://github.com/UPCnet/vilaix.santaperpetuamogoda.git',
+      license='gpl',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['vilaix', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'vilaix.core',
+          'vilaix.theme'
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ vilaix/santaperpetuamogoda/interfaces.py
@@ -0,0 +1,6 @@
+from zope.interface import Interface
+from vilaix.theme.browser.interfaces import IVilaixTheme
+
+
+class ISantaPerpetuaLayer(IVilaixTheme):
+    """Marker interface for Santa Perpetua de Mogoda Layer depending on the Vilaix one."""
